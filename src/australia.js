73110ff3db1d3c15796776b66b8144e1fa1b74e4
Martin Carlsson <martin.carlsson@omegapoint.se>
2018-11-11 21:08:07 +0100

    More wombats.

--- src/australia.js
+++ src/australia.js
@@ -1,7 +1,16 @@
-const Api = function() {
+var rp = require('request-promise');
+
+const Australia = function() {
+
+  const url = 'https://www.data.act.gov.au/resource/ymvu-tmp4.json?commonname=Common Wombat'
+
   this.wombats = function() {
-    return [];
-  }
+    return rp({
+      uri: url,
+      qs: { commonname: 'Common Wombat' },
+      json: true
+    });
+  };
 };
 
-module.exports = Api
+module.exports = Australia
--- test/australia-test.js
+++ test/australia-test.js
@@ -13,7 +13,19 @@ describe('Australia', () => {
     expect(true).to.be.true;
   });
 
-  it('should not have any wombats', () => {
-    expect(this.australia.wombats()).to.be.empty;
+  it('should not have any wombats', async () => {
+    const wombats = await this.australia.wombats();
+    expect(wombats).to.be.empty;
+  });
+
+  it('should have two wombats', async () => {
+    const wombats = await this.australia.wombats();
+    expect(wombats).to.have.length(2);
+  });
+
+  it('should have scientific name', async () =>  {
+    const wombats = await this.australia.wombats();
+    const scientificnames = wombats.map((wombat) => wombat.scientificname);
+    expect(scientificnames).to.include('Vombatus ursinus');
   });
 });
