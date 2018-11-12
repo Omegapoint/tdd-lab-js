e6f1581e2ef56d96a32357b14e0d823f9e4f9d5b
Martin Carlsson <martin.carlsson@omegapoint.se>
2018-11-12 07:34:34 +0100

    New name.

--- src/australia.js
+++ src/australia.js
@@ -1,11 +1,11 @@
-var rp = require('request-promise');
+var request = require('request-promise');
 
 const Australia = function() {
 
   const url = 'https://www.data.act.gov.au/resource/ymvu-tmp4.json?commonname=Common Wombat'
 
   this.wombats = function() {
-    return rp({
+    return request({
       uri: url,
       qs: { commonname: 'Common Wombat' },
       json: true
