7cfefcd4b07ecc8635a1e449eb00b06dacb2672c
Martin Carlsson <martin.carlsson@omegapoint.se>
2018-11-21 22:42:53 +0100

    Added simple server.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const animals = require('./animals');
+const australia = require('./australia');
+
+const app = express()
+const port = 8080
+
+app.get('/wombats', async (req, res, next) => {
+    try {
+        const wombats = await australia(animals).wombats();
+        res.json(wombats);
+    }
+    catch (e) {
+        next(e);
+    }
+})
+app.get('/wallabies', async (req, res, next) => {
+    try {
+        let wallabies = await australia(animals).wallabies();
+        res.json(wallabies);
+    }
+    catch (e) {
+        next(e);
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
