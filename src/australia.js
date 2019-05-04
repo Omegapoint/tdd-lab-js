90712ed7a422f3ebc69d6d9d1b6b252944a20e1c
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-05-04 21:58:10 +0200

    Complicating stuff

--- src/australia.js
+++ src/australia.js
@@ -1,35 +1,50 @@
-const geocoding = require('./geocoding');
 const Promise = require('bluebird');
+const request = require('request-promise');
+const geocoding = require('./geocoding');
 
-const australia = function (animals) {
+const url = 'https://www.data.act.gov.au/resource/ymvu-tmp4.json';
+
+const australia = function () {
+
+    function fetchAnimals(name) {
+        const req = request({
+            uri: url,
+            qs: {
+                '$limit': 50,
+                commonname: name
+            },
+            json: true
+        });
+        return Promise.map(req, (animal) => {
+            return {
+                commonname: animal.commonname,
+                scientificname: animal.scientificname,
+                lat: animal.northing_mga,
+                long: animal.easting_mga,
+                date: animal.recorddate,
+            };
+        }).map((animal) => {
+            return geocoding.reverse(animal.lat, animal.long).then((locations) => {
+                const location = locations.find((location) => true);
+                return {
+                    commonname: animal.commonname,
+                    scientificname: animal.scientificname,
+                    date: animal.date,
+                    state: location.state,
+                    country: location.country
+                };
+            });
+        });
+    }
+
     return {
         wombats: function () {
-            const wombats = animals.get({commonname: 'Common Wombat'});
-            return Promise.map(wombats, (animal) => {
-                    return {
-                        commonname: animal.commonname,
-                        scientificname: animal.scientificname,
-                        date: animal.date
-                    };
-                }
-            );
+            return fetchAnimals('Common Wombat');
         },
         wallabies: function () {
-            const wallabies = animals.get({commonname: 'Swamp Wallaby'});
-            return Promise.map(wallabies, (animal) => {
-                return geocoding.reverse(animal.lat, animal.long).then((locations) => {
-                    const location = locations.find((location) => true);
-                    return {
-                        commonname: animal.commonname,
-                        scientificname: animal.scientificname,
-                        date: animal.date,
-                        state: location.state,
-                        country: location.country
-                    };
-                });
-            });
+            return fetchAnimals('Swamp Wallaby');
         }
     };
 };
 
-module.exports = australia
+module.exports = australia;
