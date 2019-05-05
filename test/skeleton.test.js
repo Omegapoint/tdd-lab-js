daa109fa8189f1f5b24c929aaafe844eaae6a057
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-05-05 16:10:20 +0200

    Adding some nocking to skeleton test

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const nock = require('nock');
 const fs = require('fs');
 
 describe('The Animals in Australia API', () => {
@@ -6,6 +7,23 @@ describe('The Animals in Australia API', () => {
   let app;
 
   beforeAll(async () => {
+    nock('https://www.data.act.gov.au/resource/ymvu-tmp4.json')
+      .get(/.*/)
+      .reply(200, [])
+      .persist();
+
+    nock('http://www.datasciencetoolkit.org')
+      .get(/\/coordinates2politics\/.*/)
+      .reply(200, [{
+        "politics": [
+          {"type": "admin2", "friendly_type": "country", "name": "Australia", "code": "aus"},
+          {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
+          {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
+          {"type": "admin4", "friendly_type": "state", "name": "Australian Capital Territory", "code": "as01"}],
+        "location": {"latitude": -35.4566, "longitude": 148.89}
+      }])
+      .persist();
+
     app = await require('../src/index');
   });
 
