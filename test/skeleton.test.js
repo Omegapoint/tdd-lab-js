65e67f1b1b033ace6d2d60eef448a2c260ca25dd
Martin Carlsson <martin.carlsson@omegapoint.se>
2019-05-06 07:52:33 +0200

    Examples with query parameters.

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -4,51 +4,69 @@ const fs = require('fs');
 
 describe('The Animals in Australia API', () => {
 
-  let app;
+    let app;
 
-  beforeAll(async () => {
-    nock('https://www.data.act.gov.au/resource/ymvu-tmp4.json')
-      .get(/.*/)
-      .reply(200, [])
-      .persist();
+    beforeAll(async () => {
+        nock('https://www.data.act.gov.au/resource/ymvu-tmp4.json')
+            .get(/.*/)
+            .query({
+                '$limit': 50,
+                commonname: "Common Wombat"
+            })
+            .reply(200, [])
+            .persist();
 
-    nock('http://www.datasciencetoolkit.org')
-      .get(/\/coordinates2politics\/.*/)
-      .reply(200, [{
-        "politics": [
-          {"type": "admin2", "friendly_type": "country", "name": "Australia", "code": "aus"},
-          {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
-          {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
-          {"type": "admin4", "friendly_type": "state", "name": "Australian Capital Territory", "code": "as01"}],
-        "location": {"latitude": -35.4566, "longitude": 148.89}
-      }])
-      .persist();
+        nock('https://www.data.act.gov.au/resource/ymvu-tmp4.json')
+            .get(/.*/)
+            .query({
+                '$limit': 50,
+                commonname: "Swamp Wallaby"
+            })
+            .reply(200, [])
+            .persist();
 
-    app = await require('../src/index');
-  });
+        nock('http://www.datasciencetoolkit.org')
+            .get(/\/coordinates2politics\/.*/)
+            .reply(200, [{
+                "politics": [
+                    {"type": "admin2", "friendly_type": "country", "name": "Australia", "code": "aus"},
+                    {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
+                    {"type": "admin4", "friendly_type": "state", "name": "New South Wales", "code": "as02"},
+                    {
+                        "type": "admin4",
+                        "friendly_type": "state",
+                        "name": "Australian Capital Territory",
+                        "code": "as01"
+                    }],
+                "location": {"latitude": -35.4566, "longitude": 148.89}
+            }])
+            .persist();
 
-  afterAll((done) => {
-    app.close(done);
-  });
-
-  describe('/wallabies', () => {
-
-    it('should respond with expected wallabies', (done) => {
-      request(app).get('/wallabies').then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+        app = await require('../src/index');
     });
-  });
 
-  describe('/wombats', () => {
-
-    it('should respond with expected wombats', (done) => {
-      request(app).get('/wombats').then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+    afterAll((done) => {
+        app.close(done);
     });
-  });
+
+    describe('/wallabies', () => {
+
+        it('should respond with ok', (done) => {
+            request(app).get('/wallabies').then((response) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+
+    describe('/wombats', () => {
+
+        it('should respond with ok', (done) => {
+            request(app).get('/wombats').then((response) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
 
 });
