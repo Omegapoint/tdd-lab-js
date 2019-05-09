4c41c57e270d9290e8e39a9f19557751c0fc4a0e
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-05-09 13:59:41 +0200

    Added test responses and snapshots

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -13,7 +13,46 @@ describe('The Animals in Australia API', () => {
                 '$limit': 50,
                 commonname: "Common Wombat"
             })
-            .reply(200, [])
+            .reply(200, [
+                {
+                    "commonname": "Common Wombat",
+                    "coordinateuncertaintyinmeters": "50",
+                    "data_resource_name": "ACT Wildlife Atlas",
+                    "easting_mga": "149.11",
+                    "fid": "35606",
+                    "geodeticdatum": "GDA94",
+                    "individualcount": "1",
+                    "location_1": {
+                        "type": "Point",
+                        "coordinates": [
+                            149.11,
+                            -35.5831
+                        ]
+                    },
+                    "northing_mga": "-35.5831",
+                    "recorddate": "12/07/2015",
+                    "scientificname": "Vombatus ursinus"
+                },
+                {
+                    "commonname": "Common Wombat",
+                    "coordinateuncertaintyinmeters": "500",
+                    "data_resource_name": "ACT Wildlife Atlas",
+                    "easting_mga": "149.128",
+                    "fid": "16507",
+                    "geodeticdatum": "GDA94",
+                    "individualcount": "1",
+                    "location_1": {
+                        "type": "Point",
+                        "coordinates": [
+                            149.128,
+                            -35.2804
+                        ]
+                    },
+                    "northing_mga": "-35.2804",
+                    "recorddate": "14/01/1986",
+                    "scientificname": "Vombatus ursinus"
+                }
+            ])
             .persist();
 
         nock('https://www.data.act.gov.au/resource/ymvu-tmp4.json')
@@ -22,7 +61,46 @@ describe('The Animals in Australia API', () => {
                 '$limit': 50,
                 commonname: "Swamp Wallaby"
             })
-            .reply(200, [])
+            .reply(200, [
+                {
+                    "commonname": "Swamp Wallaby",
+                    "coordinateuncertaintyinmeters": "1000",
+                    "data_resource_name": "ACT Wildlife Atlas",
+                    "easting_mga": "149.088",
+                    "fid": "16054",
+                    "geodeticdatum": "GDA94",
+                    "individualcount": "1",
+                    "location_1": {
+                        "type": "Point",
+                        "coordinates": [
+                            149.088,
+                            -35.3992
+                        ]
+                    },
+                    "northing_mga": "-35.3992",
+                    "recorddate": "1/04/1987",
+                    "scientificname": "Wallabia bicolor"
+                },
+                {
+                    "commonname": "Swamp Wallaby",
+                    "coordinateuncertaintyinmeters": "500",
+                    "data_resource_name": "ACT Wildlife Atlas",
+                    "easting_mga": "148.856",
+                    "fid": "15961",
+                    "geodeticdatum": "GDA94",
+                    "individualcount": "1",
+                    "location_1": {
+                        "type": "Point",
+                        "coordinates": [
+                            148.856,
+                            -35.54
+                        ]
+                    },
+                    "northing_mga": "-35.54",
+                    "recorddate": "16/07/1995",
+                    "scientificname": "Wallabia bicolor"
+                }
+            ])
             .persist();
 
         nock('http://www.datasciencetoolkit.org')
@@ -54,6 +132,7 @@ describe('The Animals in Australia API', () => {
         it('should respond with ok', (done) => {
             request(app).get('/wallabies').then((response) => {
                 expect(response.statusCode).toBe(200);
+                expect(response.body).toMatchSnapshot();
                 done();
             });
         });
@@ -64,6 +143,7 @@ describe('The Animals in Australia API', () => {
         it('should respond with ok', (done) => {
             request(app).get('/wombats').then((response) => {
                 expect(response.statusCode).toBe(200);
+                expect(response.body).toMatchSnapshot();
                 done();
             });
         });
